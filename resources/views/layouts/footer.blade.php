a40985bb840f4ed50d93a5e956f7c35a524e5a5c
nyx08dreamer <nikirv0808@gmail.com>
2025-08-29 14:43:45 -0400

    Creacion de componentes

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 3.2.0
+    </div>
+    <strong>Copyright &copy; 2005-2025 <a href="https://adminlte.io">{{ env('APP_NAME') }}</a>.</strong> Todos los derechos reservados.
+</footer>
