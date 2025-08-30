86846f855db81aa28c8e5720ff1725d183d611a7
nyx08dreamer <nikirv0808@gmail.com>
2025-08-29 20:30:12 -0400

    Anexo de fuente de letra, Creacion de modelo y controlador User

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,30 @@
 <?php
 
+use App\Http\Controllers\Admin\UsersController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('page');
 });
 
-Route::get('/hola', function () {
-    return view('hola');
-});
+
+// Gestion
+    // tickets
+    //asignacion
+
+// Configuracion
+    // tipos
+    // prioridad
+
+// Reportes
+    // nuevos
+    // recientes
+
+// Administracion
+    // usuarios
+
+    Route::get('administracion/usuarios', [UsersController::class, 'index']);
+
+
+    // roles
+    // permisos (solo lectura)
